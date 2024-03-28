0136d8714853c55c1cee0427b62dac3dd8d8b5b6
zTomz <tomvogel0@gmail.com>
2024-03-28 14:01:54 +0100

    Initial commit

---
+++ code_field/lib/src/code_theme/code_theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/widgets.dart';
+
+import 'code_theme_data.dart';
+
+class CodeTheme extends InheritedWidget {
+  const CodeTheme({
+    required this.data,
+    super.key,
+    required super.child,
+  });
+
+  final CodeThemeData? data;
+
+  static CodeThemeData? of(BuildContext context) {
+    final widget = context.dependOnInheritedWidgetOfExactType<CodeTheme>();
+    return widget?.data;
+  }
+
+  @override
+  bool updateShouldNotify(CodeTheme oldWidget) {
+    return oldWidget.data != data;
+  }
+}
---
+++ dolphin_app/lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:dolphin_code_field/code_text_field.dart';
+import 'package:flutter_highlight/themes/monokai.dart';
+import 'package:highlight/languages/dart.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const CodeTheme(
+        data: CodeThemeData(
+          styles: monokaiTheme,
+        ),
+        child: MyCodeField(),
+      ),
+    );
+  }
+}
+
+class MyCodeField extends StatefulWidget {
+  const MyCodeField({super.key});
+
+  @override
+  State<MyCodeField> createState() => _MyCodeFieldState();
+}
+
+class _MyCodeFieldState extends State<MyCodeField> {
+  late final CodeController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = CodeController(
+      language: dart,
+      params: const EditorParams(
+        tabSpaces: 4,
+      ),
+      modifiers: [
+        const IndentModifier(),
+        const CurlyBracketModifier(),
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CodeField(
+        expands: true,
+        controller: controller,
+      ),
+    );
+  }
+}
+
+class CurlyBracketModifier extends CodeModifier {
+  const CurlyBracketModifier() : super('{');
+
+  @override
+  TextEditingValue? updateString(
+      String text, TextSelection sel, EditorParams params) {
+    final newString = TextEditingValue(
+      text: '$text{}',
+      selection: TextSelection.fromPosition(
+        TextPosition(offset: text.length + 1),
+      ),
+    );
+    return newString;
+  }
+}
